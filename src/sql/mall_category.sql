0346cb5f0e3f59195c76fe344b8667a9aef6451d
shipei <903812316@qq.com>
2020-08-20 00:21:42 +0800

    sql

---
+++ src/sql/mall_category.sql
@@ -0,0 +1,7 @@
+INSERT INTO `mall_category` VALUES (1, 0, '女装', 1, 1, '2020-8-19 23:18:20', '2020-8-19 23:18:24');
+INSERT INTO `mall_category` VALUES (2, 0, '男装', 1, 2, '2020-8-19 23:18:54', '2020-8-19 23:18:57');
+INSERT INTO `mall_category` VALUES (3, 1, '连衣裙', 1, 1, '2020-8-19 23:19:12', '2020-8-19 23:19:15');
+INSERT INTO `mall_category` VALUES (4, 1, '半身裙', 1, 2, '2020-8-19 23:19:35', '2020-8-19 23:19:38');
+INSERT INTO `mall_category` VALUES (5, 1, '上衣', 1, 3, '2020-8-19 23:19:58', '2020-8-19 23:20:02');
+INSERT INTO `mall_category` VALUES (6, 2, 'T恤', 1, 1, '2020-8-19 23:20:29', '2020-8-19 23:20:32');
+INSERT INTO `mall_category` VALUES (7, 2, '裤子', 1, 2, '2020-8-19 23:20:46', '2020-8-19 23:20:49');
